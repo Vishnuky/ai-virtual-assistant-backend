de5899f2ca2095af9d1c6d6c06b5dc6c14bfa2d2
Vishnu Kumar <yadvishnu7880@gmail.com>
2026-05-06 10:17:45 +0530

    Rename protect middleware to isAuth

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,28 +1,23 @@
 import jwt from "jsonwebtoken"
 
-const protect = (req, res, next) => {
+const isAuth = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
 
-    // Check header
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "No token, unauthorized" })
     }
 
     const token = authHeader.split(" ")[1]
-
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    // Attach user (IMPORTANT: match payload)
-    req.user = decoded   // {_id: ...}
+    req.userId = decoded._id
 
     next()
-
   } catch (err) {
     console.error("JWT error:", err.message)
     return res.status(401).json({ message: "Token invalid or expired" })
   }
 }
 
-export default protect
+export default isAuth
